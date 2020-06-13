cdd075e5f40424f78505d49aee7f21806129f631
X-tymchenko <xtymchenko@ex.ua>
2020-06-13 14:25:51 +0300

    remove comments from .js

--- dist/twitch-strategy.js
+++ dist/twitch-strategy.js
@@ -7,40 +7,6 @@ exports.Strategy = void 0;
 const util_1 = __importDefault(require("util"));
 const passport_oauth2_1 = __importDefault(require("passport-oauth2"));
 const node_fetch_1 = __importDefault(require("node-fetch"));
-/**
- * `Strategy` constructor.
- *
- * The Twitch authentication strategy authenticates requests by delegating to
- * Twitch using the OAuth 2.0 protocol.
- *
- * Applications must supply a `verify` callback which accepts an `accessToken`,
- * `refreshToken` and service-specific `profile`, and then calls the `done`
- * callback supplying a `user`, which should be set to `false` if the
- * credentials are not valid.  If an exception occured, `err` should be set.
- *
- * Options:
- *   - `clientID`      your Twitch application"s client id
- *   - `clientSecret`  your Twitch application"s client secret
- *   - `callbackURL`   URL to which Twitch will redirect the user after granting authorization
- *
- * Examples:
- *
- *     passport.use(new TwitchStrategy({
- *         clientID: "123-456-789",
- *         clientSecret: "shhh-its-a-secret"
- *         callbackURL: "https://www.example.net/auth/twitch/callback"
- *       },
- *       function(accessToken, refreshToken, profile, done) {
- *         User.findOrCreate(..., function (err, user) {
- *           done(err, user);
- *         });
- *       }
- *     ));
- *
- * @param {Object} options
- * @param {Function} verify
- * @api public
- */
 function Strategy(options, verify) {
     const params = Object.assign(Object.assign({}, options), { name: 'twitch', authorizationURL: 'https://id.twitch.tv/oauth2/authorize', tokenURL: 'https://id.twitch.tv/oauth2/token' });
     this.clientID = options.clientID;
@@ -50,20 +16,6 @@ function Strategy(options, verify) {
 }
 exports.Strategy = Strategy;
 util_1.default.inherits(Strategy, passport_oauth2_1.default);
-/**
- * Retrieve user profile from Twitch.
- *
- * This function constructs a normalized profile, with the following properties:
- *
- *   - `provider`         always set to `twitch`
- *   - `id`
- *   - `username`
- *   - `displayName`
- *
- * @param {String} accessToken
- * @param {Function} done
- * @api protected
- */
 Strategy.prototype.userProfile = function (accessToken, done) {
     return node_fetch_1.default('https://api.twitch.tv/helix/users', {
         method: 'GET',
@@ -84,13 +36,6 @@ Strategy.prototype.userProfile = function (accessToken, done) {
         done(error, null);
     });
 };
-/**
- * Return extra parameters to be included in the authorization request.
- *
- * @param {Object} options
- * @return {Object}
- * @api protected
- */
 Strategy.prototype.authorizationParams = function (options) {
     var params = {
         force_verify: false
